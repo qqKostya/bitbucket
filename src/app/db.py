8d2aa3abb0d5b0cd471884c3499b1e16a3b48198
qqKostya <leap74rus@gmail.com>
2022-07-19 11:04:34 +0500

    add table

--- src/app/db.py
+++ src/app/db.py
@@ -1,13 +1,23 @@
 import os
 
 from databases import Database
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData, Column, Integer, Table, String, DateTime
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SQLAlchemy
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
+employee = Table(
+    "employee",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("full name", String(50)),
+    Column("job title", String(50)),
+    Column("employment date", DateTime),
+    Column("salary", Integer),
+    Column("id chief", Integer),
+)
 
 # databases query builder
 database = Database(DATABASE_URL)
